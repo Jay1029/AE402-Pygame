99ade1c5f24b69708364127b5d7403a7fc40d5f9
Jay1029 <65231411+Jay1029@users.noreply.github.com>
2020-11-10 18:37:40 +0800

    Update untitled0hfuyyuuw8.py

--- untitled0hfuyyuuw8.py
+++ untitled0hfuyyuuw8.py
@@ -3,8 +3,7 @@ class Animal():
         self.color = color
         self.types = types
         self.HP = HP
-        self.hit = hit
-        self.drink = drink
+
     def hunt(self, time):
         self.HP = self.HP - time*5
         return self.HP
@@ -15,11 +14,11 @@ class Animal():
 
     def hit(self, time):
         self.HP = self.HP - time*5
-        return self.hit
+        return self.Hp
 
     def drink(self, time):
         self.HP = self.HP + time*10
-        return self.drink
+        return self.Hp
 
 Tiger = Animal('Yellow and Black','Cat',12000)
 print('Tiger color:',Tiger.color)
@@ -30,5 +29,5 @@ print('Tiger drink:',Tiger.drink)
 #hunt:10min
 print('After Hunt Tiger Hp',Tiger.hunt(10))
 print('After Eat Tiger Hp',Tiger.eat(20))
-print('After Tiger eat Hp',Tiger.HP(20))
+print('After Tiger eat Hp',Tiger.drink(20))
 print('After Tiger hit Hp',Tiger.hit(5))
